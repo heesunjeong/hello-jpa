cf9d9406a4d69b8b22871b97d1cd80f17384cd45
Heesun Jeong <heesunjeong@wemakeprice.com>
2020-03-18 12:16:30 +0900

    Add EntityManagerFactory

--- src/main/java/hellojpa/Main.java
+++ src/main/java/hellojpa/Main.java
@@ -1,7 +1,25 @@
 package hellojpa;
 
+import hellojpa.entity.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        Member member = new Member(1L, "정조이");
+
+        em.persist(member);
+        tx.commit();
+        em.close();
+        emf.close();
     }
 }
