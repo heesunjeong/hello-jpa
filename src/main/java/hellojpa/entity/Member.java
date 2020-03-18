1952cfe4f03e79c182efa364a4e424efb8f54d4f
Heesun Jeong <heesunjeong@wemakeprice.com>
2020-03-18 10:53:11 +0900

    Add member entity

---
+++ src/main/java/hellojpa/entity/Member.java
@@ -0,0 +1,27 @@
+package hellojpa.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Member {
+    @Id
+    private Long id;
+    private String name;
+
+    public Member() {
+    }
+
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
